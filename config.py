dc9d8eecd72baebc99fecbdc275fe5cb2d5e62fa
Woog_222 <gjwpdnr0918@gmail.com>
2025-07-22 00:20:25 +0900

    Filter structure updated & stagnance price filted added

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-SCREEN_LIMIT = 30 # number of coins to display 
+SCREEN_LIMIT = 20 # number of coins to display 
 SCREEN_WINDOW = 60 # screen window (in minutes)
 SCREEN_CANDLE_INTERVAL = "5m" # screen candle interval
+SCREEN_MIN_VOLUME = 50000000 # minimum volume to be considered (50M USDT)
--- screener.py
+++ screener.py
@@ -1,15 +1,63 @@
-
 # local
 from coin import Coin
 import config
 from binance_app import BinanceApp
 
 
+class CoinFilter:
+    """
+    Base class for all coin filters
+    """
+    def __init__(self, limit, sort_key, filter_criteria_str:str, reverse=False, exclude_key=None):
+        """
+        coins : list of Coin objects
+        limit : int # number of coins to return
+        sort_key : lambda function to sort by
+        exclude_key : lambda function to exclude by
+        reverse : bool # whether to sort in descending order
+        filter_criteria_str : str # criteria to filter by
+        """
+        self.limit = limit
+        self.sort_key = sort_key 
+        self.reverse = reverse
+        self.exclude_key = exclude_key if exclude_key is not None else lambda x: False
+        self.filter_criteria_str = filter_criteria_str
+        
+    def filter_coins(self, coins):
+        """
+        coins : list of Coin objects
+        return (list): A list of Coin objects sorted by the key
+        """
+        coins_remaining = [coin for coin in coins if not self.exclude_key(coin)]
+        self._filtered_coins = sorted(coins_remaining, key=self.sort_key, reverse=self.reverse)[:self.limit]
+        return self._filtered_coins
+    
+    def get_filtered_coins(self):
+        """
+        return (list): A list of Coin objects sorted by the key
+        """
+        if not hasattr(self, "_filtered_coins"):
+            raise AssertionError("filtered_coins is not set. Please call filter_coins() first.")
+        return self._filtered_coins
+    
+    def get_explanation(self):
+        """
+        return (str): A string explaining the filter
+        """
+        str_builder = [f"--------------{self.filter_criteria_str}--------------"] \
+            + [f"{coin.symbol} : {self.sort_key(coin):.2f}" for coin in self._filtered_coins]
+        return "\n".join(str_builder)
+    
+
 
 class Screener:
-    def __init__(self):
+    def __init__(self, filters):
+        """
+        filters : list of CoinFilter objects
+        """
         self.app = BinanceApp()
         self.coins = self.get_coins()
+        self.filters = filters
         
     def get_coins(self):
         """
@@ -21,60 +69,64 @@ class Screener:
         c = self.app.client.exchange_info()
         symbols = [x.get("symbol") for x in c.get("symbols") if x.get("quoteAsset") == "USDT" and x.get("contractType") == "PERPETUAL"]
         coins = [Coin(symbol) for symbol in symbols]
-        return [coin for coin in coins if coin.get_24h_volume() > 30000000] # over 30M USDT
+        return [coin for coin in coins if coin.get_24h_volume() > config.SCREEN_MIN_VOLUME] # over 30M USDT
 
-    def get_sorted_coins(self, key):
+    def screen(self):
         """
         key : volume, price_change  
         return (list): A list of Coin objects sorted by the key
         """
-        return sorted(self.coins, key=lambda x: getattr(x, key), reverse=True)[:config.SCREEN_LIMIT]
+        
+        filtered_coins = set()
+        # Update summary
+        with open("summary.txt", "w") as f:
+            for filter in self.filters:
+                filter.filter_coins(self.coins)
+                f.write(filter.get_explanation()); f.write('\n')
+                filtered_coins = filtered_coins | set([coin.symbol for coin in filter.get_filtered_coins()])
+                
+            f.write(f'\n-------------------------Total Coins ({len(filtered_coins)})--------------------\n')
+            for symbol in filtered_coins:
+                f.write(symbol); f.write('\n')
+            
+        print("Summary saved to summary.txt")
+            
+            
+
     
     
 if __name__ == "__main__":
-
-    screener = Screener()
-    coins_sorted_by_volume = screener.get_sorted_coins("volume")
-    coins_sorted_by_price_change = screener.get_sorted_coins("price_change")
     
-    
-    # 1. Update favorites
-    with open("favorites.txt", "r") as f:
-        old_favorites = set(f.read().splitlines())
-    new_favorites = set([coin.symbol for coin in coins_sorted_by_volume]) | set([coin.symbol for coin in coins_sorted_by_price_change])
-    favorites_added = new_favorites - old_favorites
-    favorites_removed = old_favorites - new_favorites
-    
-    with open("favorites.txt", "w") as f:
-        f.write("\n".join(new_favorites))
-    print(f"Favorites updated to favorites.txt")
-        
-    # 2. Update summary
-    with open("summary.txt", "w") as f:
+    def get_price_change(coin):
+        return (max([float(c[2]) for c in coin.klines]) - min([float(c[3]) for c in coin.klines])) / min([float(c[3]) for c in coin.klines]) * 100
 
-        # 1. volume
-        f.write("---------------------------------------------\nVolume\n---------------------------------------------\n")
-        for coin in coins_sorted_by_volume:
-            f.write(str(coin) + "\n")
-        
-        # 2. price change
-        f.write("---------------------------------------------\nPrice Change\n---------------------------------------------\n")
-        for coin in coins_sorted_by_price_change:
-            f.write(str(coin) + "\n")
-            
-        
-        # 3. Update favorites
-        f.write(f"--------Favorites added-------------\n")
-        for coin_symbol in favorites_added:
-            f.write(coin_symbol + "\n")
-        
-        f.write(f"--------Favorites removed-------------\n")
-        for coin_symbol in favorites_removed:
-            f.write(coin_symbol + "\n")
-            
-
-            
-    print("Summary saved to summary.txt")
-            
+    screener = Screener(
+        filters = [
+            # Filter to find coins with high trading volume in the last hour            
+            CoinFilter(
+                limit=config.SCREEN_LIMIT, 
+                sort_key=lambda x: sum([float(candle[7]) for candle in x.klines]), # sum up the quote volume of the last 1h
+                filter_criteria_str="1h_Volume",
+                reverse=True
+            ),
+            # Filter to find coins with drastic price change in the last hour
+            CoinFilter(
+                limit=config.SCREEN_LIMIT, 
+                sort_key=get_price_change,
+                filter_criteria_str="1h_drastic_price_change",
+                reverse=True
+            ),
+            # Filter to find coins with stagnant price change
+            CoinFilter(
+                limit=config.SCREEN_LIMIT, 
+                sort_key=get_price_change,
+                # exclude coins with over 500M USDT 24h volume and price change less than 1%
+                exclude_key=lambda x: x.get_24h_volume() > 500000000 or get_price_change(x) < 0.01, 
+                filter_criteria_str="1h_stagnant_price_change (exclude over 500M USDT 24h volume and price change less than 1%)",
+                reverse=False
+            ),  
+        ]
+    )
+    screener.screen()
             
     
